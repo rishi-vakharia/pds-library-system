7646e0951509eb00b427f00ec69a9ae3c5534279
rishi-vakharia <rishi.rvv@gmail.com>
2024-09-03 22:37:56 +0530

    Added files for v4

--- driver.c
+++ driver.c
@@ -12,18 +12,32 @@
 
 int main()
 {
-	char *repo_name = "demo";
-	libsys_create(repo_name);
+	char *stud_repo_name = "stud";
+	char *book_repo_name = "book";
+	char *issue_repo_name= "issue";
+
+	
+
 	int status;
 	struct Book testContact;
 	char *test_case_id;
 
+
+	test_case_id = "01";
+	status = libsys_create(book_repo_name,stud_repo_name,issue_repo_name);
+	if( status == LIB_SUCCESS ){
+		TREPORT(test_case_id,"SUCCESS")
+	}
+	else{
+		TREPORT(test_case_id,"FAIL")
+		exit(status);
+	}
 	testContact.isbn = 10000;
 	strcpy(testContact.title, "dummy name");
 	strcpy(testContact.author, "dummy author");
 
-	test_case_id = "01";
-	status = libsys_open( repo_name );
+	test_case_id = "02";
+	status = libsys_open(book_repo_name,stud_repo_name,issue_repo_name);
 	if( status == LIB_SUCCESS ){
 		TREPORT(test_case_id,"SUCCESS")
 	}
@@ -32,7 +46,7 @@ int main()
 		exit(status);
 	}
 
-	test_case_id = "02";
+	test_case_id = "03";
 	testContact.isbn = 10000;
 	status = put_book_by_isbn(testContact.isbn, &testContact );
 	if( status == BOOK_SUCCESS )
@@ -41,14 +55,14 @@ int main()
 		TREPORT(test_case_id,"FAIL")
 
 
-	test_case_id = "03";
+	test_case_id = "04";
 	status = get_book_by_isbn( 10000, &testContact );
 	if( status == BOOK_SUCCESS )
 		TREPORT(test_case_id,"SUCCESS")
 	else
 		TREPORT(test_case_id,"FAIL")
 
-	test_case_id = "04";
+	test_case_id = "05";
 	if( testContact.isbn == 10000  && 
 		strcmp(testContact.title,"dummy name") == 0 &&
 		strcmp(testContact.author,"dummy author") == 0 )
@@ -59,34 +73,16 @@ int main()
 	else
 		TREPORT(test_case_id,"FAIL")
 
-	test_case_id = "05";
-	status = libsys_close();
-	if( status == BOOK_SUCCESS ){
-		TREPORT(test_case_id,"SUCCESS")
-	}
-	else{
-		TREPORT(test_case_id,"FAIL")
-		exit(status);
-	}
+
 
 	test_case_id = "06";
-	status = libsys_open( repo_name );
-	if( status == LIB_SUCCESS ){
-		TREPORT(test_case_id,"SUCCESS")
-	}
-	else{
-		TREPORT(test_case_id,"FAIL")
-		exit(status);
-	}
-
-	test_case_id = "07";
 	status = get_book_by_isbn( 10000, &testContact );
 	if( status == BOOK_SUCCESS )
 		TREPORT(test_case_id,"SUCCESS")
 	else
 		TREPORT(test_case_id,"FAIL")
 
-	test_case_id = "08";
+	test_case_id = "07";
 	if( testContact.isbn == 10000  && 
 		strcmp(testContact.title,"dummy name") == 0 &&
 		strcmp(testContact.author,"dummy author") == 0 )
@@ -95,54 +91,82 @@ int main()
 	else
 		TREPORT(test_case_id,"FAIL")
 
-	test_case_id = "09";
-	status = delete_book_by_isbn( 10000 );
+
+	
+	struct Student testStudent;
+	
+
+	testStudent.rollno = 10000;
+	strcpy(testStudent.name, "dummy name");
+	strcpy(testStudent.address, "dummy address");
+
+
+
+	test_case_id = "8";
+	testStudent.rollno = 10000;
+	status = put_student_by_rollno(testStudent.rollno, &testStudent );
 	if( status == BOOK_SUCCESS )
 		TREPORT(test_case_id,"SUCCESS")
 	else
 		TREPORT(test_case_id,"FAIL")
 
-	test_case_id = "09";
-	status = delete_book_by_isbn( 10000 );
+
+	test_case_id = "9";
+	status = get_student_by_rollno( 10000, &testStudent );
 	if( status == BOOK_SUCCESS )
 		TREPORT(test_case_id,"SUCCESS")
 	else
 		TREPORT(test_case_id,"FAIL")
-
-	testContact.isbn = 10000;
-	strcpy(testContact.title, "dummy name");
-	strcpy(testContact.author, "dummy author");
 
 	test_case_id = "10";
-	testContact.isbn = 10000;
-	status = put_book_by_isbn(testContact.isbn, &testContact );
-	if( status == BOOK_SUCCESS )
+	if( testStudent.rollno == 10000  && 
+		strcmp(testStudent.name,"dummy name") == 0 &&
+		strcmp(testStudent.address,"dummy address") == 0 )
+	{
+
 		TREPORT(test_case_id,"SUCCESS")
+	}
 	else
 		TREPORT(test_case_id,"FAIL")
+
 
 
 	test_case_id = "11";
-	status = get_book_by_isbn( 10000, &testContact );
+	status = get_student_by_rollno( 10000, &testStudent );
 	if( status == BOOK_SUCCESS )
 		TREPORT(test_case_id,"SUCCESS")
 	else
 		TREPORT(test_case_id,"FAIL")
 
 	test_case_id = "12";
-	if( testContact.isbn == 10000  && 
-		strcmp(testContact.title,"dummy name") == 0 &&
-		strcmp(testContact.author,"dummy author") == 0 )
-	{
+	if( testStudent.rollno == 10000  && 
+		strcmp(testStudent.name,"dummy name") == 0 &&
+		strcmp(testStudent.address,"dummy address") == 0 )
 
 		TREPORT(test_case_id,"SUCCESS")
-	}
+	else
+		TREPORT(test_case_id,"FAIL")
+
+	
+	test_case_id = "13";
+	status = issue( 10000, 10000 );
+	if( status==LIB_SUCCESS )
+
+		TREPORT(test_case_id,"SUCCESS")
 	else
 		TREPORT(test_case_id,"FAIL")
 
 
+	test_case_id = "14";
+	status = issue( 10080, 10050 );
+	if( status==LIB_REC_NOT_FOUND )
+		TREPORT(test_case_id,"SUCCESS")
+	else
+		TREPORT(test_case_id,"FAIL")
 
-	test_case_id = "13";
+	
+
+	test_case_id = "15";
 	status = libsys_close();
 	if( status == LIB_SUCCESS ){
 		TREPORT(test_case_id,"SUCCESS")
@@ -151,6 +175,7 @@ int main()
 		TREPORT(test_case_id,"FAIL")
 		exit(status);
 	}
+
 	return 0;
 }
 
--- libsys.c
+++ libsys.c
@@ -7,14 +7,22 @@
 // Define the global variable of libsys
 struct LibsysInfo repo_handle;
 
-// Open the data file and index file in "wb" mode
-// Initialize index file by store "0" to indicate there are zero entries in index file
 
-int libsys_create( char *repo_name)
-{   
-    char dfname[30];            char ifname[30];
-    strcpy(dfname, repo_name);  strcpy(ifname, repo_name);
+//--------------------------------create()---------------------------------
+int libsys_create( char *book_name,char *stud_name,char *issue_name){
+	//assuming filename are passed without extension(.dat)
+	
+	char dfname[30];            char ifname[30];			
+    strcpy(dfname, book_name);  strcpy(ifname, book_name);
     strcat(dfname, ".dat");     strcat(ifname, ".ndx");
+
+	char sfname[30];
+	strcpy(sfname, stud_name);
+	strcat(sfname, ".dat");
+
+	char issfname[30];
+	strcpy(issfname, issue_name);
+	strcat(issfname, ".dat");
 
     
     FILE* dat_fp=fopen(dfname, "wb");
@@ -22,123 +30,133 @@ int libsys_create( char *repo_name)
         return LIB_FILE_ERROR;
     
     FILE* ndx_fp=fopen(ifname, "wb");
-    if(ndx_fp==NULL)
+    if(ndx_fp==NULL)	
+		return LIB_FILE_ERROR;
+
+	int entries=0;
+    if(fwrite(&entries, sizeof(int), 1, ndx_fp)!=1)		
+		return LIB_FILE_ERROR;
+
+
+	FILE* stud_fp=fopen(sfname, "wb");
+    if(stud_fp==NULL)
+        return LIB_FILE_ERROR;
+
+	FILE* iss_fp=fopen(issfname, "wb");
+    if(iss_fp==NULL)
         return LIB_FILE_ERROR;
     
 
-    int entries=0;
-    if(fwrite(&entries, sizeof(int), 1, ndx_fp)!=1)
-        return LIB_FILE_ERROR;
-    
+
     fclose(dat_fp);
     fclose(ndx_fp);
+	fclose(stud_fp);
+	fclose(iss_fp);
 
     return LIB_SUCCESS;
 }
 
-// Open the data file and index file in rb+ mode
-// Update the fields of LIB_RepoInfo appropriately
-// Read number of index entries from index file
-// Load index_entries array by reading index entries from the index file
-// Close only the index file
-int libsys_open( char *repo_name )
-{
-    if(repo_handle.repo_status==LIB_REPO_OPEN)
-        return LIB_REPO_ALREADY_OPEN;
 
+
+//--------------------------------open()---------------------------------
+int libsys_open(char *book_name,char *stud_name,char *issue_name){
+	
+	int s1= booksys_open(book_name);
+	if(s1!=LIB_SUCCESS)
+		return s1;
+	
+	int s2= studsys_open(stud_name);
+	if(s2!=LIB_SUCCESS)
+		return s2;
+	
+	int s3= issuesys_open(issue_name);
+	if(s3!=LIB_SUCCESS)
+		return s3;
+	
+	return LIB_SUCCESS;
+}
+
+int booksys_open( char *repo_name ){
+
+	if(repo_handle.book_repo_status==LIB_REPO_OPEN)
+        return LIB_REPO_ALREADY_OPEN;
 
     char dfname[30];            char ifname[30];
     strcpy(dfname, repo_name);  strcpy(ifname, repo_name);
     strcat(dfname, ".dat");     strcat(ifname, ".ndx");
 
 
-    FILE* ndx_fp=fopen(ifname, "rb+");
-    if(ndx_fp==NULL)
+	//initialising book system of library
+	strcpy(repo_handle.booksys_name, repo_name);			
+	repo_handle.booksys_data_fp=fopen(dfname, "rb+");
+    if(repo_handle.booksys_data_fp==NULL)
         return LIB_FILE_ERROR;
+	repo_handle.book_repo_status=LIB_REPO_OPEN;
+
+	repo_handle.libsys_ndx_fp=fopen(ifname, "rb+");
+    if(repo_handle.libsys_ndx_fp==NULL)
+        return LIB_FILE_ERROR;
+	if(fread(&repo_handle.index_count, sizeof(int), 1, repo_handle.libsys_ndx_fp)!=1)   
+        return LIB_FILE_ERROR;
+    if(fread(repo_handle.index_entries, sizeof(struct LIB_NdxInfo), repo_handle.index_count, repo_handle.libsys_ndx_fp)!=repo_handle.index_count)
+        return LIB_FILE_ERROR;
+    fclose(repo_handle.libsys_ndx_fp);  
+    return LIB_SUCCESS;
     
-    
-    strcpy(repo_handle.libsys_name, repo_name);         //initialising library
-    repo_handle.libsys_data_fp=fopen(dfname, "rb+");
-    if(repo_handle.libsys_data_fp==NULL)
-        return LIB_FILE_ERROR;
-    repo_handle.repo_status=LIB_REPO_OPEN;
-    if(fread(&repo_handle.index_count, sizeof(int), 1, ndx_fp)!=1)   
-        return LIB_FILE_ERROR;
-    if(fread(repo_handle.index_entries, sizeof(struct LIB_NdxInfo), repo_handle.index_count, ndx_fp)!=repo_handle.index_count)
-        return LIB_FILE_ERROR;
+}
 
 
-    fclose(ndx_fp);  
+int issuesys_open( char *repo_name ){
+	if(repo_handle.issue_repo_status==LIB_REPO_OPEN)
+        return LIB_REPO_ALREADY_OPEN;
+
+    char issfname[30];           
+    strcpy(issfname, repo_name);  
+    strcat(issfname, ".dat");     
+
+
+	//initialising issue system of library
+	strcpy(repo_handle.issuesys_name, repo_name);			
+	repo_handle.issuesys_data_fp=fopen(issfname, "rb+");
+    if(repo_handle.issuesys_data_fp==NULL)
+        return LIB_FILE_ERROR;
+	repo_handle.issue_repo_status=LIB_REPO_OPEN;
 
     return LIB_SUCCESS;
 }
 
 
-// put_rec_by_key
-//-----check index file for key
-//-----if key already present check for flag 
-//-----if key is present but flag is RECORD_DELETED then just add entry at same index i.e update only offset and update flag; return status
-//-----if key is present but flag is RECORD_PRSENT return failure as data is already present
-//-----if key is not prsent then proceed with following steps:
+int studsys_open( char *repo_name ){
 
-// Seek to the end of the data file
-// Create an index entry with the current data file location using ftell
-// Add index entry to array using offset returned by ftell
-// Write the key at the current data file location
-// Write the record after writing the key
-// return status
+	if(repo_handle.stud_repo_status==LIB_REPO_OPEN)
+        return LIB_REPO_ALREADY_OPEN;
 
-int put_book_by_isbn( int key, struct Book *rec )
-{
-    if(repo_handle.repo_status!=LIB_REPO_OPEN)
-        return LIB_REPO_NOT_OPEN;
-
-    int i;
-    for(i=0; i<repo_handle.index_count; i++){
-        struct LIB_NdxInfo entry=repo_handle.index_entries[i];
-        if(entry.key==key){
-			if(entry.flag==RECORD_DELETED){
-                repo_handle.index_entries[i].flag=RECORD_PRESENT;
-                fseek(repo_handle.libsys_data_fp, entry.offset, SEEK_SET);
-                fseek(repo_handle.libsys_data_fp, sizeof(int), SEEK_CUR);
-                if(fwrite(rec, sizeof(struct Book), 1, repo_handle.libsys_data_fp)!=1)
-                    return LIB_FILE_ERROR;
-                return LIB_SUCCESS;
-			}
-			else	
-				return LIB_ADD_FAILED;                      //isbn or book is already present
-        }
-    }
-
-    fseek(repo_handle.libsys_data_fp, 0, SEEK_END);     
-    int location=ftell(repo_handle.libsys_data_fp);
-
-    if(fwrite(&key, sizeof(int), 1, repo_handle.libsys_data_fp) != 1)
-		return LIB_FILE_ERROR;
-
-	if(fwrite(rec, sizeof(struct Book), 1, repo_handle.libsys_data_fp) != 1)    //add the book
-		return LIB_FILE_ERROR;
+    char sfname[30];           
+    strcpy(sfname, repo_name);  
+    strcat(sfname, ".dat");     
 
 
-    struct LIB_NdxInfo index={key, location, RECORD_PRESENT};
-    repo_handle.index_entries[repo_handle.index_count++]=index;         //add index entry
-    
-	return LIB_SUCCESS;   
-}	
+	//initialising student system of library
+	strcpy(repo_handle.studsys_name, repo_name);			
+	repo_handle.studsys_data_fp=fopen(sfname, "rb+");
+    if(repo_handle.studsys_data_fp==NULL)
+        return LIB_FILE_ERROR;
+	repo_handle.stud_repo_status=LIB_REPO_OPEN;
 
-// get_rec_by_key
-//check repo status
-// Search for index entry in index_entries array
-//-----use flag : RECORD_PRSENT to read valid entries
-// Seek to the file location based on offset in index entry
-// Read the key at the current file location 
-// Read the record after reading the key
-int get_book_by_isbn( int key, struct Book *rec )
-{
-	if(repo_handle.repo_status!=LIB_REPO_OPEN)
+    return LIB_SUCCESS;
+}
+
+
+
+
+//------------------------------book:NO change ----------------------------
+
+int get_book_by_isbn( int key, struct Book *rec ){
+
+	if(repo_handle.book_repo_status!=LIB_REPO_OPEN)
         return LIB_REPO_NOT_OPEN;
     
-    int i, location=-1;
+    int i, location = -1;
     for(i=0; i<repo_handle.index_count; i++){
         struct LIB_NdxInfo entry = repo_handle.index_entries[i];
         if(entry.key==key){
@@ -153,31 +171,64 @@ int get_book_by_isbn( int key, struct Book *rec )
     if(location == -1)
         return LIB_REC_NOT_FOUND;
     
-    fseek(repo_handle.libsys_data_fp, location, SEEK_SET);  
+    fseek(repo_handle.booksys_data_fp, location, SEEK_SET);  
     int isbn;
 
-	if(fread(&isbn, sizeof(int), 1, repo_handle.libsys_data_fp)!=1)
+	if(fread(&isbn, sizeof(int), 1, repo_handle.booksys_data_fp)!=1)
         return LIB_FILE_ERROR;
     
     if(isbn!=key)
         return LIB_REC_NOT_FOUND;
 
-    if(fread(rec, sizeof(struct Book), 1, repo_handle.libsys_data_fp)!=1)
+    if(fread(rec, sizeof(struct Book), 1, repo_handle.booksys_data_fp)!=1)
         return LIB_FILE_ERROR;
 
     return LIB_SUCCESS;
 }
 
 
-//-----delete_rec_by_key
-//-----check repo status
-//-----Search for index entry in index_entries array
-//-----if key matches and flag is RECORD_PRSENT then reset flag
-//-----if key matches but flag is RECORD_DELETED  return status LIB_REC_NOT_FOUND
+int put_book_by_isbn( int key, struct Book *rec ){
 
-int delete_book_by_isbn( int key )
-{
-	if(repo_handle.repo_status!=LIB_REPO_OPEN)
+	if(repo_handle.book_repo_status!=LIB_REPO_OPEN)
+        return LIB_REPO_NOT_OPEN;
+
+    int i;
+    for(i=0; i<repo_handle.index_count; i++){
+        struct LIB_NdxInfo entry=repo_handle.index_entries[i];
+        if(entry.key==key){
+			if(entry.flag==RECORD_DELETED){
+                repo_handle.index_entries[i].flag=RECORD_PRESENT;
+                fseek(repo_handle.booksys_data_fp, entry.offset, SEEK_SET);
+                fseek(repo_handle.booksys_data_fp, sizeof(int), SEEK_CUR);
+                if(fwrite(rec, sizeof(struct Book), 1, repo_handle.booksys_data_fp)!=1)
+                    return LIB_FILE_ERROR;
+                return LIB_SUCCESS;
+			}
+			else	
+				return LIB_ADD_FAILED;                      						//isbn or book is already present
+        }
+    }
+
+    fseek(repo_handle.booksys_data_fp, 0, SEEK_END);     
+    int location=ftell(repo_handle.booksys_data_fp);
+
+    if(fwrite(&key, sizeof(int), 1, repo_handle.booksys_data_fp) != 1)
+		return LIB_FILE_ERROR;
+
+	if(fwrite(rec, sizeof(struct Book), 1, repo_handle.booksys_data_fp) != 1)    	//add the book
+		return LIB_FILE_ERROR;
+
+
+    struct LIB_NdxInfo index={key, location, RECORD_PRESENT};
+    repo_handle.index_entries[repo_handle.index_count++]=index;         			//add index entry
+
+	return LIB_SUCCESS;   
+}
+
+
+int delete_book_by_isbn( int key ){
+
+	if(repo_handle.book_repo_status!=LIB_REPO_OPEN)
         return LIB_REPO_NOT_OPEN;
 
 	int i;
@@ -197,39 +248,159 @@ int delete_book_by_isbn( int key )
 
 
 
-// libsys_close
-// Open the index file in wb mode (write mode, not append mode)
-// Write number of index entries at the beginning of index file
-// Unload the index array into the index file (overwrite the entire index file)
-// Close the index file and data file
-int libsys_close()
-{
-	 if(repo_handle.repo_status==LIB_REPO_CLOSED)
+//-----------------------------student: additional code---------------------------
+int put_student_by_rollno( int rollno_to_write, struct Student *rec ){
+
+	if(repo_handle.stud_repo_status!=LIB_REPO_OPEN)
+		return LIB_REPO_NOT_OPEN;
+	
+	//set the file pointer to end
+	fseek(repo_handle.studsys_data_fp, 0, SEEK_END);
+
+	//Check success of fwrite - fwrite returns the no. of elements successfully written
+	if(fwrite(&rollno_to_write, sizeof(int), 1, repo_handle.studsys_data_fp) != 1)
+		return LIB_ADD_FAILED;
+	if(fwrite(rec, sizeof(struct Student), 1, repo_handle.studsys_data_fp) != 1)
+		return LIB_ADD_FAILED;
+	
+	return LIB_SUCCESS;
+       
+}
+
+int get_student_by_rollno( int rollno_to_read, struct Student *rec ){
+
+	int roll;
+
+	if(repo_handle.stud_repo_status!=LIB_REPO_OPEN)
+		return LIB_REPO_NOT_OPEN;
+	
+	//set the file pointer to start
+	fseek(repo_handle.studsys_data_fp, 0, SEEK_SET);
+	fread(&roll, sizeof(int), 1, repo_handle.studsys_data_fp);
+
+	//loop till eof is not reached - feof returns non-zero if eof is reached and 0 otherwise
+	while(feof(repo_handle.studsys_data_fp)==0){
+		if(roll==rollno_to_read){
+			fread(rec, sizeof(struct Student), 1, repo_handle.studsys_data_fp);
+			return LIB_SUCCESS;
+		}
+		//skip the record	
+		fseek(repo_handle.studsys_data_fp, sizeof(struct Student), SEEK_CUR);
+		fread(&roll, sizeof(int), 1, repo_handle.studsys_data_fp);
+	}
+	return LIB_REC_NOT_FOUND;
+}
+
+
+
+//--------------------------------issue()---------------------------------
+int issue(int rollno, int isbn){
+
+	if(repo_handle.book_repo_status!=LIB_REPO_OPEN)
+		return LIB_REPO_NOT_OPEN;
+	
+	if(repo_handle.stud_repo_status!=LIB_REPO_OPEN)
+		return LIB_REPO_NOT_OPEN;
+
+	if(repo_handle.issue_repo_status!=LIB_REPO_OPEN)
+		return LIB_REPO_NOT_OPEN;
+
+
+	struct Student s;
+	int st1= get_student_by_rollno(rollno, &s);
+	if(st1!=BOOK_SUCCESS)
+		return st1;
+
+	struct Book b;
+	int st2= get_book_by_isbn(isbn, &b);
+	if(st2!=BOOK_SUCCESS)
+		return st2;
+
+
+	struct Issue issue={rollno, isbn};
+	fseek(repo_handle.issuesys_data_fp, 0, SEEK_END);
+	if(fwrite(&issue, sizeof(struct Issue), 1, repo_handle.issuesys_data_fp) != 1)
+		return LIB_ADD_FAILED;
+
+	
+	return LIB_SUCCESS;
+}
+
+
+
+//----------------------------------close()---------------------------------
+int libsys_close(){
+
+	int s1= booksys_close();
+	if(s1!=LIB_SUCCESS)
+		return s1;
+	
+	int s2= studsys_close();
+	if(s2!=LIB_SUCCESS)
+		return s2;
+	
+	int s3= issuesys_close();
+	if(s3!=LIB_SUCCESS)
+		return s3;
+	
+	return LIB_SUCCESS;
+}
+
+int booksys_close(){
+
+	if(repo_handle.book_repo_status==LIB_REPO_CLOSED)
         return LIB_REPO_NOT_OPEN;
 
-
     char ifname[30];
-    strcpy(ifname, repo_handle.libsys_name);
+    strcpy(ifname, repo_handle.booksys_name);
     strcat(ifname, ".ndx");
 
-    
-    FILE* ndx_fp=fopen(ifname, "wb");
-    if(ndx_fp==NULL)
+
+    repo_handle.libsys_ndx_fp=fopen(ifname, "wb");
+	if(repo_handle.libsys_ndx_fp==NULL)
         return LIB_FILE_ERROR;
-
-
-    if(fwrite(&repo_handle.index_count, sizeof(int), 1, ndx_fp)!=1)         //deactivate the library
+    if(fwrite(&repo_handle.index_count, sizeof(int), 1, repo_handle.libsys_ndx_fp)!=1)         		
         return LIB_NDX_SAVE_FAILED;
-    if(fwrite(repo_handle.index_entries, sizeof(struct LIB_NdxInfo), repo_handle.index_count, ndx_fp)!=repo_handle.index_count)
+    if(fwrite(repo_handle.index_entries, sizeof(struct LIB_NdxInfo), repo_handle.index_count, repo_handle.libsys_ndx_fp)!=repo_handle.index_count)
         return LIB_NDX_SAVE_FAILED;
-    strcpy(repo_handle.libsys_name, "");
-    repo_handle.index_count=0;
-    repo_handle.repo_status=LIB_REPO_CLOSED;
-    fclose(repo_handle.libsys_data_fp);     repo_handle.libsys_data_fp=NULL;
+	
+
+	fclose(repo_handle.libsys_ndx_fp);		
+	repo_handle.libsys_ndx_fp=NULL;
+	repo_handle.index_count=0;
 
 
-	fclose(ndx_fp);
+    strcpy(repo_handle.booksys_name, "");
+    repo_handle.book_repo_status=LIB_REPO_CLOSED;
+    fclose(repo_handle.booksys_data_fp);     
+	repo_handle.booksys_data_fp=NULL;
+
 
     return LIB_SUCCESS;
 }
 
+int studsys_close(){
+
+	if(repo_handle.stud_repo_status==LIB_REPO_CLOSED)
+        return LIB_REPO_NOT_OPEN;
+
+	strcpy(repo_handle.studsys_name, "");
+	repo_handle.stud_repo_status=LIB_REPO_CLOSED;
+	fclose(repo_handle.studsys_data_fp);
+	repo_handle.studsys_data_fp=NULL;
+
+	return LIB_SUCCESS;
+}
+
+int issuesys_close(){
+
+	if(repo_handle.issue_repo_status==LIB_REPO_CLOSED)
+        return LIB_REPO_NOT_OPEN;
+
+	strcpy(repo_handle.issuesys_name, "");
+	repo_handle.issue_repo_status=LIB_REPO_CLOSED;
+	fclose(repo_handle.issuesys_data_fp);
+	repo_handle.issuesys_data_fp=NULL;
+
+	return LIB_SUCCESS;
+}
